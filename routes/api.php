10d0e512c218e5fa662600c20f8ffdc51c58ab87
Ahmad Rifai <ahmmd.riffai@gmail.com>
2024-03-26 10:58:41 +0700

    feature:add api curent login

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +18,60 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:api'])->get('/current', function (Request $request) {
+    $user = Auth::user();
+
+    //  $token->addClaim('my-claim', 'my custom claim data');
+    // $user = User::find($token->getUserIdentifier());
+
+    $role = [];
+    foreach ($user->roles as $value) {
+        $role[] = $value->name;
+    }
+
+    $permissions = [];
+    foreach($user->getAllPermissions() as $value) {
+        $permissions[] = $value->name;
+    }
+
+    $userToken = [
+        "id" => $user->id,
+        "name" => $user->name,
+        'email' => $user->email,
+        "roles" => $role,
+        "permissions" => $permissions
+    ];
+    return response()->json([
+        $userToken
+    ]);
+});
+
+Route::post('/login', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json($validator->errors(), 400);
+    }
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Login Failed!',
+        ]);
+    }
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Login Success!',
+        'data'    => $user,
+        'token'   => $user->createToken('authToken')->accessToken
+    ]);
+
 });
 
 Route::get('/logout', function(Request $request) {
